ecb944bcfcb4dc2c8da7aea44acbee17740c5c6d
qscheetz <124484154+Quentin2006@users.noreply.github.com>
2026-01-29 18:54:57 -0800

    cool shader

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,7 +1,22 @@
 #version 330 core
 out vec4 FragColor;
 
+in vec3 vPos;       // Position from vertex shader
+uniform float time; // Don't forget to send this from C++!
+
 void main()
 {
-  FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    // 1. Define two nice colors
+    vec3 pink = vec3(1.0, 0.0, 0.8);
+    vec3 cyan = vec3(0.0, 1.0, 1.0);
+
+    // 2. Calculate a "Mix Factor" (0.0 to 1.0)
+    // We take the Y position (height), add time to make it scroll, 
+    // and wrap it in sin() to make it wave back and forth.
+    float factor = sin(vPos.y * 2.0 + time) * 0.5 + 0.5;
+
+    // 3. Mix them!
+    vec3 finalColor = mix(pink, cyan, factor);
+
+    FragColor = vec4(finalColor, 1.0);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,12 +1,14 @@
 #version 330 core
-layout(location = 0) in vec3 aPos;
+layout (location = 0) in vec3 aPos;
 
-uniform mat4 model; // The object's position (World Space)
-uniform mat4 view; // The camera's position (View Space)
-uniform mat4 projection; // The perspective lens (Clip Space)
+out vec3 vPos; // Pass local position to fragment shader
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-  // The Order Matters: Projection * View * Model * Position
-  gl_Position = projection * view * model * vec4(aPos, 1.0);
+    vPos = aPos;
+    gl_Position = projection * view * model * vec4(aPos, 1.0);
 }
